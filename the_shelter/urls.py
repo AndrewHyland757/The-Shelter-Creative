f74750ebf09b3d2ed3226605237e1682099ac584
AndrewHyland757 <hyland757@gmail.com>
2024-11-20 14:54:59 +0000

    Add sitemap url

--- the_shelter/urls.py
+++ the_shelter/urls.py
@@ -4,13 +4,16 @@ from django.conf import settings
 from django.conf.urls.static import static
 from .views import handler404
 from . import views
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("home.urls")),
     path("project/", include("projects.urls")),
+    path("sitemap.xml", TemplateView.as_view(template_name="sitemaps/sitemap.xml", content_type="application/xml")),
 ]
+
 
 handler404 = "the_shelter.views.handler404"
 
