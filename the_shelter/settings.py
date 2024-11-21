9b40f516bda95fa0686270892bd69e0e5e01343f
AndrewHyland757 <hyland757@gmail.com>
2024-11-21 15:41:59 +0000

    Debug false. collect static and make migrations.

--- the_shelter/settings.py
+++ the_shelter/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
-DEBUG = True
+DEBUG = False
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
